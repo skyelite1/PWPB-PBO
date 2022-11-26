3201606996339a8baae4f16e7e771cfd6d9f21cc
Abdulloh <108718601+skyelite1@users.noreply.github.com>
2022-11-26 09:41:16 +0700

    Delete abdul directory

--- abdul/proses-data.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $id = $_GET['id'];
-    $username = $_GET['username'];
-    $password = $_GET['password'];
-
-     $koneksi = new PDO("mysql:host=localhost;dbname=abdul;",'root','');
-
-     $koneksi->query("INSERT INTO `akun`(`id`, `username`, `password`) VALUES ('$id','$username','$password')");
-?>
